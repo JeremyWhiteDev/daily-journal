6f27f7fe03dfc20bbb0b4ed7bbc5e4ae965f7171
Jeremy White <jcwhite414@gmail.com>
2022-09-03 11:02:03 -0500

    added incomplete event listener for form submit

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,39 @@
-import {getEntries} from "./entries.js"
+import { getEntries } from "./entries.js";
 //^importing journal entry duplication function
 
 //OBJECTIVE: Write a function that takes in the date from the journal entry and returns the day of the week that entry occurred. Display the day of the week in the journal entry card.
 
 const findDay = (date) => {
-    const d = new Date(date);
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",]
-    const day = daysOfWeek[d.getDay()]
-    return day;
-    }
+  const d = new Date(date);
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = daysOfWeek[d.getDay()];
+  return day;
+};
 
 const journalEntries = getEntries();
 //^create copy in the global scope of this moddule so it can be accessed with sort function...
 
 //OBJECTIVE: Greate a function that can take any array as an argument (important for sorting later on!) and that iterates over that array and "dresses it up" in HTML, inserting it on the DOM.
 const displayEntries = (array) => {
-    let htmlSection = "";
-    for (const entry of array) {
-        let entryMood = "";
-        if (entry.mood === "happy") {
-            entryMood = "😃"
-        } else if (entry.mood === "sad") {
-            entryMood = "🙁"
-        } else if (entry.mood === "curious") {
-            entryMood = "🤔"
-        }
-        htmlSection += `<section class="entry-post-card"> 
+  let htmlSection = "";
+  for (const entry of array) {
+    let entryMood = "";
+    if (entry.mood === "happy") {
+      entryMood = "😃";
+    } else if (entry.mood === "sad") {
+      entryMood = "🙁";
+    } else if (entry.mood === "curious") {
+      entryMood = "🤔";
+    }
+    htmlSection += `<section class="entry-post-card"> 
         <section class="entry-card-info">                      
         <h3>${findDay(entry.date)} 
         ${entry.date}</h3>
@@ -42,17 +50,17 @@ const displayEntries = (array) => {
         <img class="entry-btn edit-icon" src="./images/Edit Icon.png">
         <img class="entry-btn delete-icon" src="./images/Trash Can Icon.png">
         </section>
-        </section>`
-    }
-    document.getElementById("entries").innerHTML = htmlSection;
-}
+        </section>`;
+  }
+  document.getElementById("entries").innerHTML = htmlSection;
+};
 
 // displayEntries(journalEntries)
 //^Testing function
 
 //OBJECTIVE: update the application to have the form rendered from JavaScript.
 const displayForm = () => {
-    let htmlSection = `<h2>Have you made your entry today?!?</h2>
+  let htmlSection = `<h2>Have you made your entry today?!?</h2>
 
                 <fieldset class="entryField">
                     <label for="entryDate">Date of Entry</label>
@@ -79,72 +87,69 @@ const displayForm = () => {
                     </select>
                 </fieldset>
 
-                <input type="submit" value="Record Journal Entry" class="btn entryForm_btn">
-            </form>`
-            document.getElementById("entryForm").innerHTML = htmlSection;
-}
+                <input id="submitBtn" type="button"value="Record Journal Entry" class="btn entryForm_btn">
+            </form>`;
+  document.getElementById("entryForm").innerHTML = htmlSection;
+};
 
-displayForm()
+displayForm();
 //^puts the displayForm on the page right away
 
 //OBJECTIVE: create a way to filter array by mood. by the user interacting with the HTML
-const moodBtns = document.querySelectorAll(".mood-btn")
+const moodBtns = document.querySelectorAll(".mood-btn");
 
-moodBtns.forEach(btn => {
-    //add click event listener for each button that runs a function when clicked.
-    btn.addEventListener("click", (e) => {
-        //get current target (button that is clicked) data id.
-        const selectedMood = e.currentTarget.dataset.id;
-        //create a filtered array based on the clicked button
-        const entryMood = journalEntries.filter((entry) =>  {
-            if (entry.mood === selectedMood) {
-                return entry;
-            }
-        })
-        //displays original array for "all" button 
-         if (selectedMood === "all") {
-            displayEntries(journalEntries)
-        }
-        //displays filtered array
-        else {
-            displayEntries(entryMood)
-        }
-        })
-    })
+moodBtns.forEach((btn) => {
+  //add click event listener for each button that runs a function when clicked.
+  btn.addEventListener("click", (e) => {
+    //get current target (button that is clicked) data id.
+    const selectedMood = e.currentTarget.dataset.id;
+    //create a filtered array based on the clicked button
+    const entryMood = journalEntries.filter((entry) => {
+      if (entry.mood === selectedMood) {
+        return entry;
+      }
+    });
+    //displays original array for "all" button
+    if (selectedMood === "all") {
+      displayEntries(journalEntries);
+    }
+    //displays filtered array
+    else {
+      displayEntries(entryMood);
+    }
+  });
+});
 
 //OBJECTIVE: create a way to sort the entries based on mood, date, or concepts. By the user interacting with the HTML
 const sortElement = document.getElementById("sort");
 //^fetches element, so that the value of that element can be read for sorting/displaying
 
-const sortArray = () => {  
-    //if the value of sort === date, run this code
-    if (sortElement.value === "date-asc") {
-        journalEntries.sort((a,b) => {
-            return a.id - b.id
-        })
-
-    }
-    else if (sortElement.value === "date-des") {
-        journalEntries.sort((a,b) => {
-            return b.id - a.id
-        })
-
-    }
-    //if the value of sort === mood, run this code, using localeCompare to sort strings
-    else if (sortElement.value === "mood") {
-        journalEntries.sort((a,b) => {
-            return a.mood.localeCompare(b.mood)
-        })
+const sortArray = () => {
+  //if the value of sort === date, run this code
+  if (sortElement.value === "date-asc") {
+    journalEntries.sort((a, b) => {
+      return a.id - b.id;
+    });
+  } else if (sortElement.value === "date-des") {
+    journalEntries.sort((a, b) => {
+      return b.id - a.id;
+    });
+  }
+  //if the value of sort === mood, run this code, using localeCompare to sort strings
+  else if (sortElement.value === "mood") {
+    journalEntries.sort((a, b) => {
+      return a.mood.localeCompare(b.mood);
+    });
     //if the value of sort === concepts, run this code, using localeCompare to sort Strings
-    } else if (sortElement.value === "concepts") {
-        journalEntries.sort((a,b) => {
-            return a.concepts.localeCompare(b.concepts)
-        })
-    }
-    displayEntries(journalEntries)
-}
+  } else if (sortElement.value === "concepts") {
+    journalEntries.sort((a, b) => {
+      return a.concepts.localeCompare(b.concepts);
+    });
+  }
+  displayEntries(journalEntries);
+};
 
-displayEntries(journalEntries)
+displayEntries(journalEntries);
 //^puts unsorted JournalEntries on page by default.
 
 sortElement.addEventListener("change", sortArray);
@@ -152,3 +157,19 @@ sortElement.addEventListener("change", sortArray);
 
 //TODO: Think of a way to have the filtered array interact with filter options. currently, sorting then filtering works, but filtering then sorting doesn't, because the filter returns a new array that the sort fucntion doesn't know about.
 
+//add event listener for submit btn of form
+document.getElementById("submitBtn").addEventListener("click", (e) => {
+  const entryDate = document.querySelector("input[name=entryDate]")?.value;
+  const entryConcepts = document.querySelector(
+    "input[name=conceptsCovered]"
+  )?.value;
+  const entryContent = document.querySelector(
+    "textarea[name=journalEntry]"
+  )?.value;
+  const entryMood = document.querySelector("select[name=mood]")?.value;
+  console.log(entryDate, entryConcepts, entryContent, entryMood);
+});
+
+//capture the data from the form,
+//add the new entry data to the array of objects
+//display the new entry on the page.
